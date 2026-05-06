6be0d5e028224e809746b63823de09c98aaa7acb
Ognennuy Vladuka <egorakovlev038@gmail.com>
2026-05-06 21:55:09 +0300

    feat: update frontend with React+Vite structure

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -8,7 +8,8 @@ function MainPage() {
       <h1 className={styles.PageTitle}>Детский технопарк «Наукоград»</h1>
       <p className={styles.ProjectDescription}>
         Детский технопарк «Наукоград» — это уникальное образовательное пространство для развития творчества, исследовательской и проектной деятельности молодежи в прогрессивном техническом направлении. Технопарк входит в сеть детских образовательных учреждений, курируемых Департаментом предпринимательства и инновационного развития города Москвы.
-
+      </p>
+      <p className={styles.ProjectDescription}>
         Обучение проходит в аудиториях детского технопарка, полностью оборудованных необходимой современной техникой и программным обеспечением. Наша образовательная среда помогает обретать знания учащимся разных возрастов!
       </p>
       <div className={styles.SiteCatalog}>
@@ -16,10 +17,9 @@ function MainPage() {
           <h2 className={styles.SectionTitle}>Мероприятия</h2>
           
           <div className={styles.CardsBlock}>
-            <div className={styles.CardsBlock}>
-              <Card icon={<img src="/icons/Light bulb.svg" alt="" />} title="Учебный день" />
-              <Card icon={<img src="/icons/Magnifier.svg" alt="" />} title="Обзорная экскурсия" />
-            </div>
+            {/* Карточки теперь прямые потомки сетки */}
+            <Card icon={<img src="/icons/Light bulb.svg" alt="" />} title="Учебный день" />
+            <Card icon={<img src="/icons/Magnifier.svg" alt="" />} title="Обзорная экскурсия" />
           </div>
         </section>
 
@@ -27,10 +27,14 @@ function MainPage() {
           <h2 className={styles.SectionTitle}>Курсы</h2>
 
           <div className={styles.CardsBlock}>
-            <div className={styles.CardsBlock}>
-              <Card icon={<img src="" alt="" />} title="Учебный день" />
-              <Card icon={<img src="" alt="" />} title="Обзорная экскурсия" />
-            </div>
+            <Card icon={<img src="/icons/Brush.svg" alt="" />} title="Учебный день" />
+            <Card icon={<img src="/icons/Brain.svg" alt="" />} title="Обзорная экскурсия" />
+            <Card icon={<img src="/icons/Brain.svg" alt="" />} title="Учебный день" />
+            <Card icon={<img src="/icons/Gear.svg" alt="" />} title="Обзорная экскурсия" />
+            <Card icon={<img src="/icons/Screen.svg" alt="" />} title="Учебный день" />
+            <Card icon={<img src="/icons/Photo.svg" alt="" />} title="Обзорная экскурсия" />
+            <Card icon={<img src="/icons/Earth.svg" alt="" />} title="Учебный день" />
+            <Card icon={<img src="/icons/Leaf.svg" alt="" />} title="Обзорная экскурсия" />
           </div>
         </section>
       </div>
